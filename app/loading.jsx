8886f22f3f970cf89ee6ec9a8b322c879048255b
Jake <57203812+tobiadiks@users.noreply.github.com>
2024-09-09 23:25:20 +0100

    Update loading.jsx

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Loading } from '@/components/loading.jsx';
+import { Loading } from '@/components/Loading.jsx';
 import React from 'react';
 
 export default function LoadingPage() {
