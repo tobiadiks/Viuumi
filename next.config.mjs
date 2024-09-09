b47c69b7c871a206f319d5f6da3eec9f499fcdd8
Lawrence Eniola <lawrencekunmilola@gmail.com>
2024-09-09 22:59:33 +0100

    zerr

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,34 @@
-// Import necessary modules
-import path from 'path';
-
-// Combine your existing Next.js configuration settings
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     optimizeFonts: true,
     compress: true,
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '@': path.resolve(__dirname, './'),
-        };
-        return config;
+    images: {
+        domains: ['img.freepik.com'], // Add the external domain here
     },
 };
 
 export default nextConfig;
+
+
+// // Import necessary modules
+// import path from 'path';
+
+// // Combine your existing Next.js configuration settings
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     swcMinify: true,
+//     optimizeFonts: true,
+//     compress: true,
+//     webpack: (config) => {
+//         config.resolve.alias = {
+//             ...config.resolve.alias,
+//             '@': path.resolve(__dirname, './'),
+//         };
+//         return config;
+//     },
+// };
+
+// export default nextConfig;
