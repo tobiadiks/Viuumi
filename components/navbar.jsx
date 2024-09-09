4067b8761c883d4a792a9d3a82cd1bd0ad790a99
Jake <57203812+tobiadiks@users.noreply.github.com>
2024-09-09 23:49:23 +0100

    Update navbar.jsx

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-children-prop */
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react';
-import Button from './Button';
+import Button from './button';
 // import Link from 'next/link';
 import Image from 'next/image';
 import { useI18n } from '../locales/client';
